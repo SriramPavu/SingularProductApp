ee53f53431457c9d4c0ec67f811f4912fdbccffa
SriramPavu <srirampavu11@gmail.com>
2025-04-24 13:16:28 +0200

    Add files via upload

---
+++ Models/Product.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SingularProductApp.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Description { get; set; } = "";
+        public string SalePrice { get; set; } = "";
+        public string Category { get; set; } = "";
+        public string Image { get; set; } = "";
+    }
+}
---
+++ Models/ProductSales.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SingularProductApp.Models
+{
+    public class ProductSales
+    {
+        public int SaleId { get; set; }
+        public string ProductId { get; set; } = "";
+        public decimal SalePrice { get; set; }
+        public decimal SaleQty { get; set; }
+        public DateTime SaleDate { get; set; } 
+    }
+}
